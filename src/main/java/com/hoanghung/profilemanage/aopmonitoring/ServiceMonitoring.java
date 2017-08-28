71ccc113faa241f627af2525e9ec99d49622bd59
Hung Hoang <hung.hoang@axonactive.com>
2017-08-28 17:54:09 +0700

    AOP in springboot

---
+++ src/main/java/com/hoanghung/profilemanage/aopmonitoring/ServiceMonitoring.java
@@ -0,0 +1,25 @@
+package com.hoanghung.profilemanage.aopmonitoring;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by hxhung on 8/28/2017.
+ */
+@Aspect
+@Component
+public class ServiceMonitoring {
+
+    @Before("execution(* endpoint..*Controller.*(..))")
+    public void logServiceAccessBefore(JoinPoint joinPoint) {
+        System.out.println("Start: " + joinPoint);
+    }
+
+    @AfterReturning("execution(* endpoint..*Controller.*(..))")
+    public void logServiceAccessAfter(JoinPoint joinPoint) {
+        System.out.println("Completed: " + joinPoint);
+    }
+}
