96ecd58b34fc41c3804a5307ededb2fe9fe34772
Hung Hoang <hung.hoang@axonactive.com>
2018-01-30 08:35:48 +0700

    Adding

--- src/main/java/com/hoanghung/profilemanage/config/LoadConfig.java
+++
@@ -1,7 +0,0 @@
-package com.hoanghung.profilemanage.config;
-
-/**
- * Created by hxhung on 8/24/2017.
- */
-public class LoadConfig {
-}
---
+++ src/main/java/com/hoanghung/profilemanage/config/SpringSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.hoanghung.profilemanage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by hxhung on 8/24/2017.
+ */
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // Authentication : User --> Roles
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("hoanghung01").password("123456!A").roles("USER")
+                .and()
+                .withUser("admin").password("123456!A").roles("USER", "ADMIN");
+    }
+
+    // Authorization: Role --> Access
+    protected void configure(HttpSecurity httpSercury) throws Exception {
+        httpSercury.httpBasic().and().authorizeRequests()
+                .antMatchers("/profile-management/**").hasRole("USER")
+                .antMatchers("/**").hasRole("ADMIN")
+                .and()
+                .csrf().disable().headers().frameOptions().disable(); // disable for customize error page auto to HTTP 403
+    }
+}
